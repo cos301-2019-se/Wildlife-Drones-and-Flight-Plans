7abab6f83a54e29e00edca8676df2b112e24c1e5
Deane Roos <blaze.cybercell@gmail.com>
2019-04-29 17:28:54 +0200

    Generates 6 x, y co-ordinates

--- server/src/controllers/map.controller.ts
+++ server/src/controllers/map.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('map')
 export class MapController {
   @Get('random-path')
-  getRandomPath() {
-    return [5, 1, 2];
+  getRandomPath(@Query('top') top: number, @Query('left') left: number, @Query('bottom') bottom: number, @Query('right') right: number) {
+    var lPoints = [];
+    var y = -1.0;
+    var x = -1.0;
+    for (let i = 0; i < 6; i++) {
+      y = (Math.random() * (top - bottom)) + (bottom - 0); // do not question me!
+      x = (Math.random() * (right - left)) + (left - 0); // do not question me!
+      lPoints.push({x, y});
+      // console.log((Math.random() * (top - bottom)) + bottom);
+    }
+    // console.log(lPoints);
+    return lPoints;
   }
 }
