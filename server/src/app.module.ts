54718b8af21c1093a73187b58650916b06b7a2cd
Reinhardt <reinhardt.eiselen@gmail.com>
2019-05-04 14:05:50 +0200

    Did conflict resolution

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
+//import { AppController } from './app.controller';
 import {MapController} from './controllers/map.controller';
-import { AppService } from './app.service';
+//import { AppService } from './app.service';
 import { MapUpdaterService } from './providers/map-updater.service';
 import { ShortestPathService } from './providers/shortest-path.service'; 
 import { DatabaseService } from './services/db.service';
@@ -11,8 +11,8 @@ import { UserController } from './controllers/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, MapController,UserController],
-  providers: [AppService, MapUpdaterService, ShortestPathService,DatabaseService,UserService],
+  controllers: [MapController,UserController],
+  providers: [MapUpdaterService, ShortestPathService,DatabaseService,UserService],
 
 })
 export class AppModule {}
