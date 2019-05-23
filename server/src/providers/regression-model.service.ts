4432725d84a2e00c65a34bc328a09c0249993532
Viidas96 <u16217498@tuks.co.za>
2019-05-23 16:12:46 +0200

    Conflict
    Chnages to conflicts

--- server/src/providers/regression-model.service.ts
+++ server/src/providers/regression-model.service.ts
@@ -13,35 +13,24 @@ export class RegressionModel {
     }
     // Trains the model with the inputs
     public trainModel(inputs, output) {
-        try {
             this.model = new MLR(inputs, output);
             console.log(JSON.stringify(this.model));
             this.log('Model has been trained');
-        } catch (err){
-            throw err;
-        }
+
     }
 
     // Prediction of the model
     public predict(inputs) {
-        try {
             const predictions = this.model.predict(inputs);
             this.log('Model has predicted');
             return predictions;
-        } catch (err) {
-            throw err;
-        }
     }
 
     // Saves a model to HDD 
     public saveModel(modelName) {
-        try {
         const jsonModel = JSON.stringify(this.model.toJSON());
         fs.writeFileSync('ai_models/' + modelName + '.json', jsonModel);
         this.log('Model has been saved');
-        } catch (err) {
-            throw err;
-        }
     }
 
     // Loads a model from storage
