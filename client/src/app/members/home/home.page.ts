0d7f2095cf8fcede808d1ad306a16945c2094138
EvansMatthew97 <evans.matthew97@gmail.com>
2019-06-09 23:36:19 +0200

    Changes to map layout

--- client/src/app/members/home/home.page.ts
+++ client/src/app/members/home/home.page.ts
@@ -1,10 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { Draw, MapOptions, Control, tileLayer, geoJSON, Map,
-  polyline, icon, FeatureGroup } from 'leaflet';
-import 'leaflet-draw';
+import { MapOptions, tileLayer, geoJSON, Map } from 'leaflet';
 import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
 import { MapService } from '../../services/map/map.service';
 import { AuthenticationService } from '../../services/authentication.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -14,22 +13,22 @@ export class HomePage {
   private map: Map;
   @ViewChild('leaflet') leaflet: LeafletDirective;
 
-  shortestPath;
-  points = [];
-  pointsLayer: FeatureGroup = new FeatureGroup();
-  shortestPathLayer = null;
-
   constructor(
     private mapService: MapService,
     private authService: AuthenticationService,
   ) {}
 
   mapOptions: MapOptions = {
+    zoomSnap: 0.3,
+    zoomAnimation: true,
+    fadeAnimation: true,
+    worldCopyJump: true,
     layers: [
-      // alternative https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
-      tileLayer('http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+      tileLayer('http://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
         maxZoom: 18,
-        attribution: null
+        attribution: 'Google Earth',
+        detectRetina: true,
       })
     ],
     zoom: 13,
@@ -39,9 +38,11 @@ export class HomePage {
   async mapReady(map: Map) {
     this.map = map;
 
+    this.map.zoomControl.setPosition('bottomright');
+
     setTimeout(() => {
       this.map.invalidateSize();
-    }, 0);
+    }, 500);
 
     const mapData = await this.mapService.getMap();
     if (!mapData.reserve) {
@@ -50,7 +51,8 @@ export class HomePage {
 
     console.log(mapData);
 
-    const reserve = geoJSON(mapData.reserve as any, {
+    // render reserve
+    geoJSON(mapData.reserve as any, {
       style: feature => {
         return {
           color: 'red',
@@ -59,141 +61,13 @@ export class HomePage {
         };
       }
     }).addTo(map);
+  }
 
-    const maxDistance = mapData.grid.reduce((max, cell) => {
-      if (cell.properties.distances.dams > max) {
-        return cell.properties.distances.dams;
-      }
-      return max;
-    }, -Infinity);
+  setMarkerAddingState() {
 
-    console.log(maxDistance);
-
-    mapData.grid = mapData.grid.map(cell => {
-      return {
-        ...cell,
-        properties: {
-          ...cell.properties,
-          distanceToWater: 1 - cell.properties.distances.dams / maxDistance,
-        }
-      };
-    });
-
-    console.log(maxDistance);
-
-    mapData.grid.forEach(cell => geoJSON(cell as any, {
-      style: feature => {
-        return {
-          color: '#000',
-          fillColor: 'purple',
-          fillOpacity: Math.pow(cell.properties.distanceToWater, 2),
-        };
-      }
-    }).addTo(map));
-
-    mapData.roads.forEach(road => geoJSON(road as any, {
-      style: feature => {
-        return {
-          weight: 1,
-          color: 'orange'
-        };
-      }
-    }).addTo(map));
-
-    mapData.dams.forEach(dam => geoJSON(dam as any, {
-      style: feature => {
-        return {
-          color: 'blue',
-          fillColor: 'blue',
-          fillOpacity: 1
-        };
-      }
-    }).addTo(map));
-
-    mapData.rivers.forEach(river => geoJSON(river as any, {
-      style: feature => {
-        return {
-          color: 'blue',
-          fillColor: 'blue',
-          fillOpacity: 1
-        };
-      }
-    }).addTo(map));
-
-    mapData.intermittentWater.forEach(water => geoJSON(water as any, {
-      style: feature => {
-        return {
-          color: 'grey',
-          fillColor: 'grey',
-          fillOpacity: 0.7,
-          dashArray: 10,
-        };
-      }
-    }).addTo(map));
-
-    mapData.residential.forEach(residence => geoJSON(residence as any, {
-      style: feature => {
-        return {
-          color: 'grey',
-          fillColor: 'grey',
-          fillOpacity: 0.7,
-        };
-      }
-    }).addTo(map));
-
-    map.addLayer(this.pointsLayer);
-    const drawControl = new Control.Draw({
-      position: 'bottomleft',
-      edit: {
-        featureGroup: this.pointsLayer,
-        remove: false,
-        edit: false
-      },
-      draw: {
-        marker: {
-          icon: icon({
-            iconSize: [25, 41],
-            iconAnchor: [13, 41],
-            iconUrl: 'assets/marker-icon.png',
-            shadowUrl: 'assets/marker-shadow.png'
-          })
-        },
-        circlemarker: false,
-        polyline: false,
-        rectangle: false,
-        circle: false,
-        polygon: false,
-      }
-    });
-
-    map.on(Draw.Event.CREATED, async e => {
-      const layer = (e as any).layer;
-      console.log(e);
-      console.log(this.pointsLayer);
-
-      this.points.push([layer._latlng.lat, layer._latlng.lng]);
-      this.pointsLayer.addLayer(layer);
-
-      console.log('this points', this.points);
-
-      const shortestPath: any[] = await this.authService.post('map/shortest-path', {
-        points: this.points, 
-      }) as any;
-
-      console.log(shortestPath);
-
-      if (this.shortestPathLayer) {
-        this.map.removeLayer(this.shortestPathLayer);
-      }
-      this.shortestPathLayer = polyline(shortestPath, {
-        color: 'yellow',
-        weight: 2
-      });
-
-      this.map.addLayer(this.shortestPathLayer);
-    });
-
-    map.addControl(drawControl);
   }
 
 }
+
+
+interface MapState {}
