34984a074107d4413d7d3394fa60dcffffae40aa
Viidas96 <u16217498@tuks.co.za>
2019-06-17 14:15:34 +0200

    Animal Classificatin
    Added animal classifiaction classes
    Changed training on animal regression

--- server/src/services/model-training.service.ts
+++ server/src/services/model-training.service.ts
@@ -12,18 +12,7 @@ export class ModelTraining {
     model.enableLogs(true);
     // AnimalLocationService
     const data = await this.animalLocationService.getAllAnimalsLocationTableData();
-    const jsonData = JSON.parse(JSON.stringify(data)).filter(
-      elephant => elephant.animalId !== 'AM105',
-    );
-    // const outputData = jsonData.filter(animal => (parseInt(animal.id) % 2) != 1).map(animal => [
-    //     parseFloat(animal.latitude), parseFloat(animal.longitude),
-    // ]);
-    // const inputData = jsonData.filter(animal => animal.id < jsonData.length && (parseInt(animal.id) % 2) != 0).map(animal => [
-    //     parseFloat(animal.latitude), parseFloat(animal.longitude), parseInt(animal.month), parseInt(animal.time),
-    //     parseInt(animal.temperature), parseFloat(animal.distanceToRivers), parseFloat(animal.distanceToDams),
-    //     parseFloat(animal.distanceToRoads), parseFloat(animal.distanceToResidences), parseFloat(animal.distanceToIntermittentWater),
-    //     parseFloat(animal.altitude),
-    // ]);
+    const jsonData = JSON.parse(JSON.stringify(data));
 
     const inputData = jsonData.map(animal => [
       parseFloat(animal.latitude),
@@ -89,31 +78,5 @@ export class ModelTraining {
         predictions,
       }),
     );
-
-    // const outputData = jsonData.filter(animal => (parseInt(animal.id) % 2) != 1 && animal.animalId == 'AM105').map(animal => [
-    //     parseFloat(animal.latitude), parseFloat(animal.longitude),
-    // ]);
-    // const inputData =  jsonData.filter(animal => animal.id < jsonData.length && (parseInt(animal.id) % 2) != 0
-    //                                             && animal.animalId == 'AM105').map(animal => [
-    //     parseFloat(animal.latitude), parseFloat(animal.longitude), parseInt(animal.month), parseInt(animal.time),
-    //     parseInt(animal.temperature), parseFloat(animal.distanceToRivers), parseFloat(animal.distanceToDams),
-    //     parseFloat(animal.distanceToRoads), parseFloat(animal.distanceToResidences), parseFloat(animal.distanceToIntermittentWater),
-    //     parseFloat(animal.altitude),
-    // ]);
-    // const random = Math.floor(Math.random() * 3000) + 1;
-    // const predictions = [];
-    // for(var i = random; i < random + 10; i++)
-    // {
-    //     var inputs = [];
-    //     inputs.push(inputData[i][0]);
-    //     inputs.push(inputData[i][1]);
-    //     const data = {
-    //         input:  inputs,
-    //         output: outputData[i],
-    //         prediction: model.predict(inputData[i]),
-    //     }
-    //     predictions.push(data);
-    // }
-    // return JSON.parse(JSON.stringify(predictions));
   }
 }
