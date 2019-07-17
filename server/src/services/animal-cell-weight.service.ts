e8acf079e88726175f9e50b9c186e92e6c2562b1
Andreas <u15048366@tuks.co.za>
2019-07-17 12:34:46 +0200

    Update animal-cell-weight.service.ts
    Made so that cells are updated if already in table.
    Added function description.
    Removed redundant function addAnimalCellWeight.

--- server/src/services/animal-cell-weight.service.ts
+++ server/src/services/animal-cell-weight.service.ts
@@ -7,76 +7,25 @@ import { Species } from '../entity/animal-species.entity';
 
 @Injectable()
 export class AnimalCellWeightService {
-  constructor(private readonly databaseService: DatabaseService) { }
+  constructor(private readonly databaseService: DatabaseService) {}
 
-  // add cell data to map cell data table in database.
-  async addAnimalCellWeight(
-    mapCellId: number,
-    speciesId: number,
-    weight0: number,
-    weight120: number,
-    weight240: number,
-    weight360: number,
-    weight480: number,
-    weight600: number,
-    weight720: number,
-    weight840: number,
-    weight960: number,
-    weight1080: number,
-    weight1200: number,
-    weight1320: number,
-    weight1440: number,
-  ): Promise<boolean> {
-    const con = await this.databaseService.getConnection();
-    const animalCellWeight = new AnimalCellWeight();
-
-    const mapCellIdExist = await con
-      .getRepository(MapCellData)
-      .findOne({ id: mapCellId });
-
-    const speciesIdExist = await con
-      .getRepository(Species)
-      .findOne({ id: speciesId });
-
-    if (mapCellIdExist == undefined || speciesIdExist == undefined) {
-      console.log('animal species or map cell does not exist');
-      return false;
-    }
-
-    try {
-      animalCellWeight.cell = mapCellIdExist;
-      animalCellWeight.species = speciesIdExist;
-      animalCellWeight.time0Weight = weight0;
-      animalCellWeight.time120Weight = weight120;
-      animalCellWeight.time240Weight = weight240;
-      animalCellWeight.time360Weight = weight360;
-      animalCellWeight.time480Weight = weight480;
-      animalCellWeight.time600Weight = weight600;
-      animalCellWeight.time720Weight = weight720;
-      animalCellWeight.time840Weight = weight840;
-      animalCellWeight.time960Weight = weight960;
-      animalCellWeight.time1080Weight = weight1080;
-      animalCellWeight.time1200Weight = weight1200;
-      animalCellWeight.time1320Weight = weight1320;
-
-      // tslint:disable-next-line:no-console
-      const addedAnimalCellWeight = await con
-        .getRepository(AnimalCellWeight)
-        .save(animalCellWeight);
-      console.log('Saved animal cell weight with id: ' + animalCellWeight.id);
-      return addedAnimalCellWeight != null;
-    } catch (error) {
-      console.log(error);
-      console.log('Animal cell weight was not saved');
-      return false;
-    }
-  }
+ /**
+  * Description - Adds animal cell weights to animalCellWeights table, 
+  *               returns true if data successfully inserted and false if 
+  *               data not inserted.
+  * Usage - the parameter cellData is an Json object that looks as follows
+  *         {"cellId":1,"speciesId":1,"time0Weight":0.5,"time120Weight":0.5
+  *         ,"time240Weight":0.5,"time360Weight":0.5,"time480Weight":0.5
+  *         ,"time600Weight":0.5,"time720Weight":0.5,"time840Weight":0.5
+  *         ,"time960Weight":0.5,"time1080Weight":0.5,"time1200Weight":0.5
+  *         ,"time1320Weight":0.5}
+  * @param cellData 
+  * @returns true|false
+  */
 
   async addAnimalCellsWeight(cellData): Promise<boolean> {
     const con = await this.databaseService.getConnection();
-    const animalCellWeight = new AnimalCellWeight();
 
-    let passed = true;
     const mapCellIdExist = await con
       .getRepository(MapCellData)
       .findOne({ id: cellData.cellId });
@@ -87,38 +36,69 @@ export class AnimalCellWeightService {
 
     if (mapCellIdExist == undefined || speciesIdExist == undefined) {
       console.log('animal species or map cell does not exist');
-      passed = false;
       return false;
     }
 
-    try {
-      animalCellWeight.cell = mapCellIdExist;
-      animalCellWeight.species = speciesIdExist;
-      animalCellWeight.time0Weight = cellData.weight0;
-      animalCellWeight.time120Weight = cellData.weight120;
-      animalCellWeight.time240Weight = cellData.weight240;
-      animalCellWeight.time360Weight = cellData.weight360;
-      animalCellWeight.time480Weight = cellData.weight480;
-      animalCellWeight.time600Weight = cellData.weight600;
-      animalCellWeight.time720Weight = cellData.weight720;
-      animalCellWeight.time840Weight = cellData.weight840;
-      animalCellWeight.time960Weight = cellData.weight960;
-      animalCellWeight.time1080Weight = cellData.weight1080;
-      animalCellWeight.time1200Weight = cellData.weight1200;
-      animalCellWeight.time1320Weight = cellData.weight1320;
-      // tslint:disable-next-line:no-console
-      const addedAnimalCellWeight = await con
-        .getRepository(AnimalCellWeight)
-        .save(animalCellWeight);
-      console.log('Saved animal cell weight with id: ' + animalCellWeight.id);
-      return addedAnimalCellWeight != null;
-    } catch (error) {
-      console.log(error);
-      console.log('Animal cell weight was not saved');
-      passed = false;
-      return false;
-    }
+    const cellWeight = await con
+      .getRepository(AnimalCellWeight)
+      .findOne({ cell: mapCellIdExist, species: speciesIdExist });
 
-    return passed;
+    if (cellWeight == undefined) {
+      const animalCellWeight = new AnimalCellWeight();  
+      try {
+        animalCellWeight.cell = mapCellIdExist;
+        animalCellWeight.species = speciesIdExist;
+        animalCellWeight.time0Weight = cellData.weight0;
+        animalCellWeight.time120Weight = cellData.weight120;
+        animalCellWeight.time240Weight = cellData.weight240;
+        animalCellWeight.time360Weight = cellData.weight360;
+        animalCellWeight.time480Weight = cellData.weight480;
+        animalCellWeight.time600Weight = cellData.weight600;
+        animalCellWeight.time720Weight = cellData.weight720;
+        animalCellWeight.time840Weight = cellData.weight840;
+        animalCellWeight.time960Weight = cellData.weight960;
+        animalCellWeight.time1080Weight = cellData.weight1080;
+        animalCellWeight.time1200Weight = cellData.weight1200;
+        animalCellWeight.time1320Weight = cellData.weight1320;
+        // tslint:disable-next-line:no-console
+        const addedAnimalCellWeight = await con
+          .getRepository(AnimalCellWeight)
+          .save(animalCellWeight);
+        // console.log('Saved animal cell weight with id: ' + animalCellWeight.id);
+        return addedAnimalCellWeight != null;
+      } catch (error) {
+        console.log(error);
+        console.log('Animal cell weight was not saved');
+        return false;
+      }
+    }
+    else{
+      try {
+        cellWeight.cell = mapCellIdExist;
+        cellWeight.species = speciesIdExist;
+        cellWeight.time0Weight = cellData.weight0;
+        cellWeight.time120Weight = cellData.weight120;
+        cellWeight.time240Weight = cellData.weight240;
+        cellWeight.time360Weight = cellData.weight360;
+        cellWeight.time480Weight = cellData.weight480;
+        cellWeight.time600Weight = cellData.weight600;
+        cellWeight.time720Weight = cellData.weight720;
+        cellWeight.time840Weight = cellData.weight840;
+        cellWeight.time960Weight = cellData.weight960;
+        cellWeight.time1080Weight = cellData.weight1080;
+        cellWeight.time1200Weight = cellData.weight1200;
+        cellWeight.time1320Weight = cellData.weight1320;
+        // tslint:disable-next-line:no-console
+        const updatedAnimalCellWeight = await con
+          .getRepository(AnimalCellWeight)
+          .save(cellWeight);
+         //console.log('updated animal cell weight with id: ' + updatedAnimalCellWeight.id);
+        return updatedAnimalCellWeight != null;
+      } catch (error) {
+        console.log(error);
+        console.log('Animal cell weight was not updated');
+        return false;
+      }
+    }
   }
 }
