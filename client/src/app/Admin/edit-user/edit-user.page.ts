a1c5f6d7721e9cb95f0847ee92fffd0abc546ad7
Viidas96 <u16217498@tuks.co.za>
2019-08-15 08:40:21 +0200

    Edit Changes to UI
    Extra button and alert prompt added

--- client/src/app/Admin/edit-user/edit-user.page.ts
+++ client/src/app/Admin/edit-user/edit-user.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Events } from '@ionic/angular';
 import { UsersService } from '../../services/users.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.page.html',
@@ -9,25 +10,80 @@ import { UsersService } from '../../services/users.service';
 })
 export class EditUserPage implements OnInit {
   user:any;
+  tempUser:any;
   error;
-  constructor(private userService:UsersService,public events: Events,private router: Router) { }
+  constructor(private alertCtrl:AlertController,private userService:UsersService,public events: Events,private router: Router) { }
 
   ngOnInit() {
     this.user = this.router.getCurrentNavigation().extras.state.user;
+    this.tempUser = JSON.parse(JSON.stringify(this.user));
+  }
+async displayAlert(editUser)
+{
+  let alert = await this.alertCtrl.create({
+    message: 'Do you wish to commit these changes?',
+    buttons: [
+      {
+        text: 'No',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Yes',
+        handler: async () => {
+          console.log('Yes clicked');
+          await this.userService.updateUser(editUser);
+          this.events.publish('user:updated');
+          this.router.navigate(['admin-tabs/users']);
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
+  checkChanges()
+  {
+    let changes = false;
+    if(this.user.name != this.tempUser.name)
+    {
+      changes = true;
+    }
+    else if(this.user.surname != this.tempUser.surname)
+    {
+      changes = true;
+    }
+    else if(this.user.email != this.tempUser.email)
+    {
+      changes = true;
+    }
+    else if(this.user.jobType != this.tempUser.jobType)
+    {
+      changes = true;
+    }
+    return changes;
   }
 
   async goBack()
   {
     const editUser = {
-      id:this.user.id,
-      name:this.user.name,
-      surname:this.user.surname,
-      email:this.user.email,
-      job:this.user.jobType
-    }   
-    await this.userService.updateUser(editUser);
-    this.events.publish('user:updated');
-    this.router.navigate(['admin-tabs/users']);
+      id:this.tempUser.id,
+      name:this.tempUser.name,
+      surname:this.tempUser.surname,
+      email:this.tempUser.email,
+      job:this.tempUser.jobType
+    }
+    if(this.checkChanges())
+    {
+      await this.displayAlert(editUser);
+    }
+    else
+    {
+      //await this.userService.updateUser(editUser);
+      //this.events.publish('user:updated');
+      this.router.navigate(['admin-tabs/users']);
+    }
   }
 
   
