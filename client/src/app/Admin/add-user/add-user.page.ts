55d34c5a7ab54293f7c63e5569bc080eb6361bd6
BCybercell <blaze.cybercell@gmail.com>
2019-08-19 11:06:48 +0200

    admin ui changes

--- client/src/app/Admin/add-user/add-user.page.ts
+++ client/src/app/Admin/add-user/add-user.page.ts
@@ -3,52 +3,59 @@ import { UsersService } from '../../services/users.service';
 import { AuthenticationService } from './../../services/authentication.service';
 import { Router } from '@angular/router';
 import { Events } from '@ionic/angular';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.page.html',
   styleUrls: ['./add-user.page.scss'],
 })
 export class AddUserPage implements OnInit {
-  name:string = "";
-  surname:string = "";
-  jobType:string = "";
-  email:string = "";
-  password:string = "";
-  confirmPassword:string = "";
   error;
-  constructor(public events: Events,private userService:UsersService,private router: Router,private auth:AuthenticationService) { }
+
+  formGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    jobType: new FormControl('pilot', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+  }, {
+    validators: [
+      (group: FormGroup) => group.get('password').value === group.get('confirmPassword').value
+        ? null : { passwordsDoNotMatch: true },
+    ]
+  });
+
+  constructor(
+    public events: Events,
+    private userService: UsersService,
+    private router: Router,
+    private auth: AuthenticationService
+  ) { }
 
   ngOnInit() {
   }
 
-  async addUser()
-  {
-    if(this.name == "" || this.surname == "" || this.email == "" || this.jobType == "" || this.password == "")
-    {
-      //Dont do anything
-      this.error = "Not all fields have been filled";
-      
-    }
-    else if(this.password != this.confirmPassword)
-    {
-      //Password dont match
-      this.error = "Password do not match";
-    }
-    else if(!this.auth.passRequirements(this.password))
-    {
-      this.error = "Password must be 8 characters and contain atleast 1 uppercase,1 lowercase,special character and a digit";
-    }
-    else{
-      //Insert
-      const addUser = {
-        id:0,
-        name:this.name,
-        surname:this.surname,
-        email:this.email,
-        job:this.jobType
+  async addUser() {
+    if (!this.formGroup.valid) {
+      // dont do anything
+      this.error = 'Not all fields have been filled';
+    } else if (this.formGroup.get('password').value !== this.formGroup.get('confirmPassword').value) {
+      // password dont match
+      this.error = 'Passwords do not match';
+    } else if (!this.auth.passRequirements(this.formGroup.get('password').value)) {
+      this.error = 'Password must be 8 characters and contain atleast 1 uppercase,1 lowercase,special character and a digit';
+    } else {
+      // insert
+      const user = {
+        id: 0,
+        name: this.formGroup.get('name').value,
+        surname: this.formGroup.get('surname').value,
+        email: this.formGroup.get('email').value,
+        job: this.formGroup.get('jobType').value,
       };
 
-      await this.userService.addUser(addUser,this.password);
+      await this.userService.addUser(user, this.formGroup.get('password').value);
       this.events.publish('user:updated');
       this.router.navigate(['admin-tabs/users']);
     }
